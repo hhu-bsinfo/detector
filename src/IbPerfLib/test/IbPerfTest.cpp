909117a487cccc8ae3341d4a280f924d44203e0b
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2018-05-26 14:51:02 +0200

    IbPerfLib/test: Add IbPerfTest

---
+++ src/IbPerfLib/test/IbPerfTest.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018 Heinrich-Heine-Universitaet Duesseldorf,
+ * Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <csignal>
+#include <chrono>
+#include <thread>
+#include "IbPerfLib/IbFabric.h"
+
+bool isRunning = true;
+
+static void SignalHandler(int signal) {
+    if(signal == SIGINT) {
+        isRunning = false;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    IbPerfLib::IbFabric fabric;
+
+    fabric.ResetCounters();
+
+    signal(SIGINT, SignalHandler);
+
+    while(isRunning) {
+        fabric.RefreshCounters();
+        std::cout << fabric;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+}
